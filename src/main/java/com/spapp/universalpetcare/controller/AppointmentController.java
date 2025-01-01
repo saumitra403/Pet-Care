cc0f495e96ed81bba621b9e9917290127fb61295
saumitra403 <mishra.saumitra06@gmail.com>
2025-01-01 23:47:02 +0530

    Added Controllers for appointment crud operations

--- src/main/java/com/spapp/universalpetcare/controller/AppointmentController.java
+++ src/main/java/com/spapp/universalpetcare/controller/AppointmentController.java
@@ -1,6 +1,8 @@
 package com.spapp.universalpetcare.controller;
 
+import com.spapp.universalpetcare.exception.ResourceNotFoundException;
 import com.spapp.universalpetcare.model.Appointment;
+import com.spapp.universalpetcare.request.AppointmentUpdateRequest;
 import com.spapp.universalpetcare.response.ApiResponse;
 import com.spapp.universalpetcare.service.appointment.AppointmentService;
 import com.spapp.universalpetcare.utils.FeedBackMessage;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,11 +31,67 @@ public class AppointmentController {
         }
     }
 
-    public ResponseEntity<ApiResponse> bookAppointment(@RequestBody Appointment appointment, @RequestParam Long senderId, @RequestParam Long recipientId) {
+    @PostMapping("/book-appointment")
+    public CompletableFuture<ResponseEntity<ApiResponse>> bookAppointment(@RequestBody Appointment appointment, @RequestParam Long senderId, @RequestParam Long recipientId) {
+        return appointmentService.bookAppointmentAsync(appointment, senderId, recipientId).handle((savedAppointment, ex) -> {
+            if (ex == null) {
+                return ResponseEntity.status(HttpStatus.CREATED)
+                        .body(new ApiResponse("Appointment scheduled successfully.", savedAppointment));
+            } else {
+                if (ex.getCause() instanceof ResourceNotFoundException) {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                            .body(new ApiResponse(ex.getMessage(), null));
+                } else {
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                            .body(new ApiResponse(ex.getMessage(), null));
+                }
+            }
+        });
+    }
+
+    @GetMapping("/appointment/{id}")
+    public ResponseEntity<ApiResponse> getAppointmentById(@PathVariable Long id) {
         try {
-
+            Appointment appointment = appointmentService.getAppointmentById(id);
+            return ResponseEntity.ok(new ApiResponse(FeedBackMessage.FOUND, appointment));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
         } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 
+    @GetMapping("/appointment/{appointmentNo}")
+    public ResponseEntity<ApiResponse> getAppointmentByNo(@PathVariable String appointmentNo) {
+        try {
+            Appointment appointment = appointmentService.getAppointmentByNo(appointmentNo);
+            return ResponseEntity.ok(new ApiResponse(FeedBackMessage.FOUND, appointment));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @DeleteMapping("/appointment/{id}")
+    public ResponseEntity<ApiResponse> deleteAppointmentById(@PathVariable Long id) {
+        try {
+            appointmentService.deleteAppointment(id);
+            return ResponseEntity.ok(new ApiResponse(FeedBackMessage.DELETE_SUCCESS, null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PutMapping("/appointment/{id}")
+    public ResponseEntity<ApiResponse> updateAppointment(@PathVariable Long id, @RequestBody AppointmentUpdateRequest request) {
+        try {
+            Appointment appointment = appointmentService.updateAppointment(id, request);
+            return ResponseEntity.ok(new ApiResponse(FeedBackMessage.UPDATE_SUCCESS, appointment));
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(new ApiResponse(e.getMessage(), null));
         }
     }
 }
--- src/main/java/com/spapp/universalpetcare/utils/FeedBackMessage.java
+++ src/main/java/com/spapp/universalpetcare/utils/FeedBackMessage.java
@@ -1,8 +1,8 @@
 package com.spapp.universalpetcare.utils;
 
 public class FeedBackMessage {
-    public static final String SUCCESS = "User created successfully";
-    public static final String UPDATE_SUCCESS = "User updated successfully";
-    public static final String FOUND = "Found";
+    public static final String SUCCESS = "Resource created successfully";
+    public static final String UPDATE_SUCCESS = "Resource updated successfully";
+    public static final String FOUND = "Resource Found";
     public static final String DELETE_SUCCESS = "Resource deleted successfully";
 }
